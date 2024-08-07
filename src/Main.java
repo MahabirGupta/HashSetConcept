961a340f92a23cdcfe4945d4c925deacb1c2d639
MahabirGupta <mahabirg81@gmail.com>
2024-08-07 11:40:13 +0800

    use addAll removeAll method and find the union, intersecction and subsets of sets

--- src/Main.java
+++ src/Main.java
@@ -46,5 +46,62 @@ public class Main {
             System.out.println(iterator.next());
         }
 
+        System.out.println();
+
+        HashSet<Integer> evenNumbers = new HashSet<>();
+        evenNumbers.add(2);
+        evenNumbers.add(4);
+        evenNumbers.add(6);
+        evenNumbers.add(8);
+        evenNumbers.add(0);
+        System.out.println(evenNumbers);
+
+        HashSet<Integer> numbers = new HashSet<>();
+//        using addAll() method
+        numbers.addAll(evenNumbers);
+        numbers.add(10);
+        System.out.println(numbers);
+
+// using removeAll() method to remove a group of element
+        numbers.removeAll(evenNumbers);
+        System.out.println(numbers);
+
+//        union of 2 sets
+        HashSet<Integer> set1 = new HashSet<>();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(4);
+        set1.add(5);
+
+        System.out.println(set1);
+
+        HashSet<Integer> set2 = new HashSet<>();
+        set2.add(3);
+        set2.add(4);
+        set2.add(5);
+//        set2.add(9);
+//        set2.add(10);
+        System.out.println(set2);
+
+//        union of 2 sets to remove duplicates
+//        set1.addAll(set2);
+//        System.out.println(set1);
+
+//        intersection of 2 sets to get the common element
+//        set1.retainAll(set2);
+//        System.out.println(set1);
+
+//        finding the different elements in Set 1 and Set 2
+//        set1.removeAll(set2);
+//        System.out.println(set1);
+
+//        getting the subset of the set
+        set1.containsAll(set2); //set2 is a subset of set1
+        System.out.println(set1.containsAll(set2));
+//        System.out.println(set1);
+
+
+
     }
 }
